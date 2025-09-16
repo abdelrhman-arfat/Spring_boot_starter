2dfdc041a02a545507cc23b63151eddfbffdb470
boody <abdoarfat2006@gmail.com>
2025-09-16 21:50:30 +0300

    setup spring boot , redis,posgress and rabbitMQ with docker project

---
+++ src/main/java/com/project/handly/HandlyApplication.java
@@ -0,0 +1,13 @@
+package com.project.handly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlyApplication.class, args);
+    }
+
+}
